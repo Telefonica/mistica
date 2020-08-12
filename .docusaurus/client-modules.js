a1cd8c814870bd4819e205665ac2156973ac1372
Yayo <yceballost@gmail.com>
2020-08-12 16:52:08 +0200

    delete docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/yayo/GitHub/mistica/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yayo/GitHub/mistica/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yayo/GitHub/mistica/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yayo/GitHub/mistica/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/yayo/GitHub/mistica/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/yayo/GitHub/mistica/src/css/custom.css"),
-];
