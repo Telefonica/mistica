781b5496c4f59b92aca2d755e4a59d9d4d436856
Yayo <yceballost@gmail.com>
2020-09-03 15:05:34 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -138,13 +138,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Telefonica/mistica/edit/master/website/',
+            'https://github.com/Telefonica/mistica/edit/master/mistica-site/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Telefonica/mistica/edit/master/website/blog/',
+            'https://github.com/Telefonica/mistica/edit/master/mistica-site/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
